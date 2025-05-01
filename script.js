0d0b7814a5fa8355f12d7b4a7850b28ff3ae2b8a
ririsahu <ririsahu1600@gmail.com>
2025-05-01 21:27:10 +0530

    navbar

---
+++ script.js
@@ -0,0 +1,37 @@
+var renav = document.querySelector("#resnav")
+
+var cross = document.querySelector("#resnav i")
+var menu = document.querySelector("nav i")
+
+var tl = gsap.timeline()
+
+
+tl.to("#resnav", {
+    right: "0%",
+    duration: 0.6,
+ })
+ tl.from("#resnav h4 , #resnav button",{
+   x:150,
+   duration:0.6,
+   
+   opacity:0,
+   stagger: 0.2
+})
+
+tl.from("#resnav i",{
+   opacity:0
+})
+ tl.pause()
+
+
+
+menu.addEventListener("click", function(){
+   renav.style.display = "block" 
+   tl.play()
+})
+cross.addEventListener("click", function(){
+   renav.style.display = "none" 
+   
+    tl.reverse()
+ })
+ 
