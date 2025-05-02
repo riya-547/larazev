36f2d988d7f217120999b47165a022badf4b9eca
ririsahu <ririsahu1600@gmail.com>
2025-05-03 01:29:33 +0530

    page4 completed

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ function resnavanimation(){
 }
 resnavanimation();
 
- 
 function navAnimation(){
    let nav = document.querySelector("nav")
 
@@ -91,6 +90,7 @@ tl.to(".nav-bottom",{
   })
 }
 navAnimation();
+
  function page2Animation(){
    var rightElems = document.querySelectorAll(".right-elem")
 
@@ -118,3 +118,28 @@ navAnimation();
    })
  }
 page2Animation()
+
+
+function page3VideoAnimation() {
+   var page3Center = document.querySelector(".page3-center")
+   var video = document.querySelector("#page3 video")
+
+   page3Center.addEventListener("click", function () {
+       video.play()
+       gsap.to(video, {
+           transform: "scaleX(1) scaleY(1)",
+           opacity: 1,
+           borderRadius: 0
+       })
+   })
+   video.addEventListener("click", function () {
+       video.pause()
+       gsap.to(video, {
+           transform: "scaleX(0.7) scaleY(0)",
+           opacity: 0,
+           borderRadius: "30px"
+       })
+   })
+}
+page3VideoAnimation()
+
